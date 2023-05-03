0bce4618fe9435675313da08b7ccc28ab2916f5c
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-05-03 23:51:22 +0500

    object passing in function

--- oop/passObjectParam.cpp
+++ oop/passObjectParam.cpp
@@ -20,7 +20,19 @@ int main(){
 
     //pass by value
     printCircle(c)
+
+    //pass by reference
+    printCircle1(c)
+
+    //pass by reference with ppointer
+    printCircle2(&c)
 }
 void printCircle(Circle c){
     cout<<"The Area is "<<c.getArea()<<endl;
+}
+void printCircle1(Circle &c){
+    cout<<"The Area is "<<c.getArea()<<endl;
+}
+void printCircle2(Circle *c){
+    cout<<"The Area is "<<c->getArea()<<endl;
 }
