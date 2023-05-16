aa16cbb18c318ddb712646bb6ddca7bf0fce31ac
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-05-16 21:50:20 +0500

    Update arrayRotation

--- dsa/arrays/arrayRotation.cpp
+++ dsa/arrays/arrayRotation.cpp
@@ -10,7 +10,31 @@ void PrintTheArray(int arr[], int n)
     }
 }
 
+void Rotate(int arr[], int d, int n)
+{
+    // Storing rotated version of array
+    int temp[n];
 
+    // Keeping track of the current index
+    int k = 0;
+
+    // array arr[] to the front of temp[]
+    for (int i = d; i < n; i++) {
+        temp[k] = arr[i];
+        k++;
+    }
+
+    // Storing the first d elements of array arr[]
+    for (int i = 0; i < d; i++) {
+        temp[k] = arr[i];
+        k++;
+    }
+
+    // Copying the elements of temp[] in arr[]
+    for (int i = 0; i < n; i++) {
+        arr[i] = temp[i];
+    }
+}
 
 
 int main()
@@ -19,6 +43,7 @@ int main()
     int N = sizeof(arr) / sizeof(arr[0]);
     int d = 2;
 
+    Rotate(arr, d, N);
     PrintTheArray(arr, N);
 
     return 0;
