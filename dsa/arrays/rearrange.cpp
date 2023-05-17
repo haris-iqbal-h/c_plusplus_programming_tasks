349cfd9d9bee3460ad20a59300cf0c65bc97e50b
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-05-17 23:13:31 +0500

    library update

--- dsa/arrays/rearrange.cpp
+++ dsa/arrays/rearrange.cpp
@@ -1,7 +1,3 @@
-// A C++ program to put positive
-// numbers at even indexes (0, 2, 4,..)
-// and negative numbers at odd
-// indexes (1, 3, 5, ..)
 #include <iostream>
 using namespace std;
 
@@ -13,18 +9,9 @@ class GFG
 	void printArray(int [],int);
 };
 
-// The main function that rearranges
-// elements of given array. It puts
-// positive elements at even indexes
-// (0, 2, ..) and negative numbers
-// at odd indexes (1, 3, ..).
 void GFG :: rearrange(int arr[], int n)
 {
-	// The following few lines are
-	// similar to partition process
-	// of QuickSort. The idea is to
-	// consider 0 as pivot and
-	// divide the array around it.
+
 	int i = -1;
 	for (int j = 0; j < n; j++)
 	{
@@ -35,20 +22,10 @@ void GFG :: rearrange(int arr[], int n)
 		}
 	}
 
-	// Now all positive numbers are at
-	// end and negative numbers at the
-	// beginning of array. Initialize
-	// indexes for starting point of
-	// positive and negative numbers
-	// to be swapped
 	int pos = i + 1, neg = 0;
 
-	// Increment the negative index by
-	// 2 and positive index by 1,
-	// i.e., swap every alternate negative
-	// number with next positive number
 	while (pos < n && neg < pos &&
-					arr[neg] < 0)
+		arr[neg] < 0)
 	{
 		swap(&arr[neg], &arr[pos]);
 		pos++;
@@ -56,8 +33,6 @@ void GFG :: rearrange(int arr[], int n)
 	}
 }
 
-// A utility function
-// to swap two elements
 void GFG :: swap(int *a, int *b)
 {
 	int temp = *a;
@@ -65,14 +40,12 @@ void GFG :: swap(int *a, int *b)
 	*b = temp;
 }
 
-// A utility function to print an array
 void GFG :: printArray(int arr[], int n)
 {
 	for (int i = 0; i < n; i++)
 		cout << arr[i] << " ";
 }
 
-// Driver Code
 int main()
 {
 	int arr[] = {-1, 2, -3, 4,
@@ -83,6 +56,3 @@ int main()
 	test.printArray(arr, n);
 	return 0;
 }
-
-// This code is contributed
-// by vt_Yogesh Shukla 1
