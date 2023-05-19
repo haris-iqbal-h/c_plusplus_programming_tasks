ab313337c154b3ffe91fc41ac28d7e279f9fa11f
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-05-19 22:48:34 +0500

    Small and Second Small in Array

---
+++ examples/smallAndSecondSmall.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int array[100], i, n;
+    cout << "Enter number of elements in the array: ";
+    cin >> n;
+    cout << "\nEnter array: ";
+    for (i = 0; i < n; i++) {
+        cin >> array[i];
+    }
+    //sorting the array
+    sort(array, array + n);
+    //first two elements are the result
+    cout << "Smallest number is:  " << array[0] << "\nSecond smallest number is " << array[1] << endl;
+    return 0;
+}
