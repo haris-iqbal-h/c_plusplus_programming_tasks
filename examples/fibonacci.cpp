9f481186b8a30842ac26ddf3878fef70ca150c9a
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-05-20 22:00:41 +0500

    Fibonacci series implement

---
+++ examples/fibonacci.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,no, first=0, second=1, next;
+
+    first=0;
+    second=1;
+
+    cout<<"How many terms u want to Display :: ";
+    cin>>no;
+
+    cout<<"\nThe Fibonacci series for [ "<<no<<" ] terms are :: \n\n";
+    for(i=0; i<no; i++)
+    {
+        cout<<" "<<first<<" ";
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    cout<<"\n";
+
+    return 0;
+}
