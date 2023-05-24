9e4fe0584d42149eb21c487c3e195cd49c1a6282
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-05-24 22:09:52 +0500

    LCM HCF Implementation

---
+++ examples/lCMHCF.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+# include <string>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<< "Enter two nos :"<<endl;
+    cout<< "Enter first no. : ";
+    cin>>a;
+    cout<< "Enter sec. no. : ";
+    cin>>b;
+    c=a*b;
+    while(a!=b)
+        {
+        if(a>b)
+            a=a-b;
+        else
+            b=b-a;
+        }
+    cout<< "HCF = " << a<<endl;
+    cout<< "LCM = " << c/a<<endl;
+    return 0;
+}
