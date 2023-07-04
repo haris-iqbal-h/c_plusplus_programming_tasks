95359578d0b9b8cdd1ea813611876222960a4195
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-07-04 22:53:36 +0500

    reverse queue implementation

---
+++ dsa/queue/reverseQueue.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+#include <stack>
+
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+class Queue {
+  private: int front;
+  int rear;
+  int arr[MAX_SIZE];
+  public: Queue() {
+    front = -1;
+    rear = -1;
+  }
+  bool isFull() {
+    return (rear == MAX_SIZE - 1);
+  }
+  bool isEmpty() {
+    return (front == -1 && rear == -1);
+  }
+  void enqueue(int x) {
+    if (isFull()) {
+      cout << "Error: Queue is full" << endl;
+      return;
+    }
+    if (isEmpty()) {
+      front = 0;
+      rear = 0;
+    } else {
+      rear++;
+    }
+    arr[rear] = x;
+  }
+  void dequeue() {
+    if (isEmpty()) {
+      cout << "Error: Queue is empty" << endl;
+      return;
+    }
+    if (front == rear) {
+      front = -1;
+      rear = -1;
+    } else {
+      front++;
+    }
+  }
+  int peek() {
+    if (isEmpty()) {
+      cout << "Error: Queue is empty" << endl;
+      return -1;
+    }
+    return arr[front];
+  }
+  void display() {
+    if (isEmpty()) {
+      cout << "Error: Queue is empty" << endl;
+      return;
+    }
+    cout << "Queue elements are: ";
+    for (int i = front; i <= rear; i++) {
+      cout << arr[i] << " ";
+    }
+    cout << endl;
+  }
+
+  void reverseQueue(Queue & q) {
+    if (q.isEmpty()) {
+      cout << "Error: Queue is empty" << endl;
+      return;
+    }
+    stack < int > s;
+    while (!q.isEmpty()) {
+      s.push(q.peek());
+      q.dequeue();
+    }
+    while (!s.empty()) {
+      q.enqueue(s.top());
+      s.pop();
+    }
+  }
+};
+
+int main() {
+  cout << "Initialize a Queue." << endl;
+  Queue q;
+  cout << "\nInsert some elements into the queue:" << endl;
+  q.enqueue(1);
+  q.enqueue(2);
+  q.enqueue(3);
+  q.enqueue(4);
+  q.enqueue(5);
+  q.display();
+  q.reverseQueue(q);
+  cout << "\nReverse Queue elements:\n";
+  q.display();
+  return 0;
+}
